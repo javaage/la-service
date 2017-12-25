c3b25aec635c4bd5e785c8713b021d76df42023b
javaage <subing.xiao@pactera.com>
2017-12-25 16:13:15 +0800

    modify

--- src/main/java/com/cisco/la/AppConfiguration.java
+++ src/main/java/com/cisco/la/AppConfiguration.java
@@ -12,60 +12,6 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class AppConfiguration {
-//	@Value("${security.oauth2.client.accessTokenUri}")
-//    private String accessTokenUri;
-//
-//    @Value("${security.oauth2.client.userAuthorizationUri}")
-//    private String userAuthorizationUri;
-//
-//    @Value("${security.oauth2.client.clientId}")
-//    private String clientID;
-//
-//    @Value("${security.oauth2.client.clientSecret}")
-//    private String clientSecret;
-//    
-//    @Value("${security.oauth2.client.pre-established-redirect-uri}")
-//    private String preEstablishedRedirectUri;
-//    
-//    @Value("${security.oauth2.client.registered-redirect-uri}")
-//    private String registeredRedirectUri;
-//    
-//    @Value("${security.oauth2.client.use-current-uri}")
-//    private Boolean useCurrentUri;
-
-//    @Bean
-//    public OAuth2RestOperations restTemplate(OAuth2ClientContext oauth2ClientContext) {
-//    	System.out.println("restTemplate");
-//    	System.out.println(oauth2ClientContext.getAccessTokenRequest().getCurrentUri());
-//    	
-//        return new OAuth2RestTemplate(resource(), oauth2ClientContext);
-//    }
-//
-//    private OAuth2ProtectedResourceDetails resource() {
-//    	Oauth2ResourceDetail resource = new Oauth2ResourceDetail();
-//        resource.setClientId(clientID);
-//        resource.setClientSecret(clientSecret);
-//        resource.setAccessTokenUri(accessTokenUri);
-//        resource.setUserAuthorizationUri(userAuthorizationUri);
-//        resource.setScope(Arrays.asList("read"));
-//        
-//        resource.setUseCurrentUri(useCurrentUri);
-//        resource.setPreEstablishedRedirectUri(preEstablishedRedirectUri);
-//
-//        return resource;
-//    }
-//	
-//    @Bean
-//    public AuthenticationSuccessHandler successHandler() {
-//		return new OAuth2SuccessHandler();
-//	}
-    
-//    @Bean
-//	public OAuth2ClientContextFilter oauth2ClientContextFilter() {
-//		OAuth2ClientContextFilter filter = new OAuth2RedirectFilter();
-//		return filter;
-//	}
-    
 	@Bean
     public PlatformTransactionManager txManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
--- src/main/java/com/cisco/la/Application.java
+++ src/main/java/com/cisco/la/Application.java
@@ -5,19 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.cisco.la")
-@EnableCaching
 @EnableTransactionManagement
-//@EnableOAuth2Sso
-//@EnableOAuth2Client
 @SpringBootApplication
 public class Application  {
 	
